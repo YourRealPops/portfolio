853a20e0298e0ef48e8ce17a4d7b20fa7d5aa24d
Adekoya Emmanuel <97315230+YourRealPops@users.noreply.github.com>
2024-07-21 21:32:54 +0100

    skills section done

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,19 +1,62 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageContentHeader from "../../components/pageContentHeader";
+import { skillsData } from "./skillsdata";
+import { Animate, AnimateKeyframes } from "react-simple-animate";
+import { Line } from "rc-progress";
+import './styles.scss'
 
-
-const Skills=()=>{
-    return(
-        <section id="skills" className="skills">
-          
-          <PageContentHeader 
-             headerText="My Skills"
-             icon= {<BsInfoCircleFill size={40}/>}
-          />
-
-       </section>
-    )
-}
+const Skills = () => {
+  return (
+    <section id="skills" className="skills">
+      <PageContentHeader
+        headerText="My Skills"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="skills__content-wrapper">
+        {skillsData.map((item, i) => (
+          <div key={i} className="skills__content-wrapper__inner-content">
+            <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{
+                transform: "translateX(-200px)",
+              }}
+              end={{
+                transform: "translateX(0px)",
+              }}
+            >
+              <h3 className="skills__content-wrapper__inner-content__text-category">
+                {item.label}
+              </h3>
+              <div>
+                {item.data.map((skillItem, j) => (
+                  <AnimateKeyframes
+                    play
+                    duration={1}
+                    keyframes={["opacity: 1", "opacity : 0"]}
+                    iterationCount="1"
+                  >
+                    <div className="progressbar-wrapper" key={j}>
+                      <p>{skillItem.skillName}</p>
+                      <Line
+                        percent={skillItem.percentange}
+                        strokeWidth="2"
+                        strokeColor="var(--yellow-th-main-color)"
+                        trailWidth={"2"}
+                        strokeLinecap="square"
+                      />
+                    </div>
+                  </AnimateKeyframes>
+                ))}
+              </div>
+            </Animate>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
---
+++ src/containers/skills/skillsdata.js
@@ -0,0 +1,73 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentange: "90",
+            },
+            {
+                skillName: "CSS",
+                percentange: "70",
+            },
+            {
+                skillName: "SCSS",
+                percentange: "60",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentange: "75",
+            },
+            {
+                skillName: "TYPESCRIPT",
+                percentange: "40",
+            },
+            {
+                skillName: "REACTJS",
+                percentange: "70",
+            },
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "PHP",
+                percentange: "20",
+            },
+            {
+                skillName: "PYTHON",
+                percentange: "30",
+            },
+            {
+                skillName: "NEXTJS",
+                percentange: "5",
+            },
+            {
+                skillName: "C++",
+                percentange: "30",
+            },
+        ],
+    },
+    {
+        label: "DATABASE",
+        data: [
+            {
+                skillName: "MYSQL",
+                percentange: "30",
+            },
+            {
+                skillName: "MONGO DB",
+                percentange: "10",
+            },
+            {
+                skillName: "B QUERY",
+                percentange: "5",
+            },
+            {
+                skillName: "ORACLE",
+                percentange: "5",
+            },
+        ],
+    }
+]
